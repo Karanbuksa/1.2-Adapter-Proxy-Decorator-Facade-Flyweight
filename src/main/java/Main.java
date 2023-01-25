c517458982fdd7a7dde9a1b1ad32c8896bb1d57f
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2023-01-26 01:34:57 +0500

    1.2.2 Двоичный фасад

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Ints intsCalc = new IntsCalculator();
-        System.out.println(intsCalc.sum(2, 2));
-        System.out.println(intsCalc.sum(10, 22));
-        System.out.println(intsCalc.pow(2, 10));
+        BinOps bins = new BinOps();
+        System.out.println(bins.mult("11100010", "111000101"));
+        System.out.println(bins.sum("11100010", "111000101"));
     }
 }
